536458d9b9cf5c990741be5e1e974b161e9dfdc8
bich.pham2 <bich.pham@mservice.com.vn>
2021-05-30 15:26:24 +0700

    - update

--- pages/components/index.js
+++ pages/components/index.js
@@ -1,7 +1,9 @@
 import QualityInput from '../components/qualityinput'
 import ListProduct from '../components/listproduct'
+import HeaderCpn from '../components/headercpn'
 
 export {
   QualityInput,
-  ListProduct
+  ListProduct,
+  HeaderCpn
 }
